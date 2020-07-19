c2e043c07647182123437d2a2235780dda8c1808
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-07-18 18:53:30 -0700

    project filtering and animation are added.

--- main.js
+++ main.js
@@ -44,9 +44,34 @@ document.addEventListener('scroll', () => {
   }
 });
 
-// When the user clicks on the button, scroll to the top
 arrowUp.addEventListener('click', () => {
   scrollIntoViews('#home');
+});
+
+//Handle Project filters
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (event) => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter === null) {
+    return;
+  }
+
+  projectContainer.classList.add('anim-out');
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
 });
 
 const scrollIntoViews = (section) => {
